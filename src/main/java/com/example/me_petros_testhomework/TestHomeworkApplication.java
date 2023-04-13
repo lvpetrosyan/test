548622670d793e53abd7e232508ab483dad7e810
lvpetrosyan <liana.liil@mail.ru>
2023-04-13 20:48:29 +0300

    com

---
+++ src/main/java/com/example/me_petros_testhomework/TestHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.me_petros_testhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/me_petros_testhomework/TestHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.me_petros_testhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
